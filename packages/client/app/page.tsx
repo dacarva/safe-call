6f185e6900e40d46b94be507fc32bffff55667d5
bogos <andre.neyra@gmail.com>
2024-08-24 23:41:54 -0300

    add worldcoin first login

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -1,10 +1,15 @@
-'use client'
+"use client";
 import { useUser } from "@/components/Context/UserContext";
 import Welcome from "./src/Sections/Welcome";
+import { SignIn } from "@/components/SignIn";
 
 export default function Home() {
+  const { user } = useUser();
 
-  return (
-    <Welcome />
-  );
+  console.log("user", user);
+  if (!user) {
+    return <SignIn />;
+  }
+
+  return <Welcome />;
 }
--- packages/client/components/SignIn/index.tsx
+++ packages/client/components/SignIn/index.tsx
@@ -1,20 +1,25 @@
 "use client";
+import Welcome from "@/app/src/Sections/Welcome";
+import { Button } from "@chakra-ui/react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export const SignIn = () => {
   const { data: session } = useSession();
+
+  if (!session) {
+    signIn();
+  }
+
   if (session) {
     return (
       <>
-        Signed in as {session?.user?.name?.slice(0, 10)} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  } else {
-    return (
-      <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
+        <Welcome />
+
+        {/* This will be removed for the final version */}
+        <div className="fixed top-0 ">
+          Signed in as {session?.user?.name?.slice(0, 10)} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
       </>
     );
   }
