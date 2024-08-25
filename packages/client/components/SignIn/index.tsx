787536e2bd52b13c28801c5d34b052792800d88a
bogos <andre.neyra@gmail.com>
2024-08-25 05:29:43 -0300

    remove logout

--- packages/client/components/SignIn/index.tsx
+++ packages/client/components/SignIn/index.tsx
@@ -14,10 +14,10 @@ export const SignIn = () => {
     return (
       <>
         {/* This will be removed for the final version */}
-        <div className="fixed top-0 ">
+        {/* <div className="fixed top-0 ">
           Signed in as {session?.user?.name?.slice(0, 10)} <br />
           <button onClick={() => signOut()}>Sign out</button>
-        </div>
+        </div> */}
         <Welcome />
       </>
     );
